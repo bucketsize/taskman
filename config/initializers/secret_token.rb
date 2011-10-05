b5aae01c97292a115edd1a23010a0cd030b7b775
jb <jb@fury.gmbh>
2011-10-05 22:55:51 +0530

    init task_man 2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TaskMan::Application.config.secret_token = '7c3582d3d5186e56eb8ce8137bbe102a9b9901b8feadc40195ee28195c160da2f32bec3f5d921f7ed6ae0a006cb7ac16ba5a2608f4d3e1e1192d31ec10f56a32'
---
+++ db/migrate/20111004123925_add_task_node1.rb
@@ -0,0 +1,7 @@
+class AddTaskNode1 < ActiveRecord::Migration
+  def self.up
+  end
+
+  def self.down
+  end
+end
