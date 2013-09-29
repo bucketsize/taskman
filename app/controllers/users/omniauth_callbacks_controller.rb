d926c12751a7657daf58c2d2e0cd2c6658ad1681
bucket.size <bucket.size@gmail.com>
2013-09-29 11:33:25 +0530

    heroku jruby prod

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,7 +7,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      session["devise.user_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
   end
@@ -20,7 +20,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
         sign_in_and_redirect @user, :event => :authentication
       else
-        session["devise.google_data"] = request.env["omniauth.auth"]
+        session["devise.user_data"] = request.env["omniauth.auth"]
         redirect_to new_user_registration_url
       end
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def user_name
+    session["devise.user_data"][:info][:name]    
+  end
+
+  def user_email
+    session["devise.user_data"][:info][:email]    
+  end
+    
 end
